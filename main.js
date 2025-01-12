09bc301a5120894f48392cfd72bad40169dfc553
juliabush <j.elizabethbush@gmail.com>
2025-01-12 17:46:26 +0100

    Finishing touches of project

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-const form = document.querySelector('.website-email__form')
-const input = document.querySelector('.email-form__input')
+const form = document.querySelector('.website__email-form');
+const button = document.querySelector('.website__submit-button');
+const msg = 'Please provide a valid email address';
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (input.value === '' || input.value == null ){
-        messages.push('Email Address is required')
+const error = document.createElement('p');
+error.classList.add('error_text');
+error.textContent = msg;
+
+function checkEmail (e) {
+    e.preventDefault();
+    const email = e.target.querySelector('.email-form__input').value;
+    if(!validateEmail(email)) {
+        form.classList.add('error');
+        form.insertBefore(error, button);
+    } else {
+        form.classList.remove('error');
+        form.removeChild(error);
     }
-    e.preventDefault()
-})
-
-function validateEmail(email) {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // A basic email regex
-    return emailPattern.test(email);
+    console.log(email);
 }
 
+function validateEmail (email) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+form.addEventListener('submit', checkEmail);
